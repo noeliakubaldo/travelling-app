93ef32b643ab999a620104a5c9d93a64746f0931
Alexandra Vegas <alexvegasdev@gmail.com>
2025-03-21 16:56:57 -0500

    refactor: :zap: update api route

--- app/screens/InformacionScreen.tsx
+++ app/screens/InformacionScreen.tsx
@@ -25,7 +25,7 @@ export default function InformacionScreen() {
 
   const fetchDestinoById = async (id: string) => {
     try {
-      const response = await fetch(`https://tu-api.com/api/destinos/${id}`);
+      const response = await fetch(`https://test-travell.azurewebsites.net/api/flights/${id}`);
       const data = await response.json();
       setVuelo(data);
     } catch (err) {
